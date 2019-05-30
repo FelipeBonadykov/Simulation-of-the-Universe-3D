dd55efe565a709f2d6c5288e479360eedbcc9357
JavaDeveloperr <31167757+JavaDeveloperr@users.noreply.github.com>
2019-05-30 11:07:00 -0400

    Add files via upload

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,8 @@
 package application;
 
-import java.io.IOException;
-import java.time.LocalTime;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,20 +11,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application 
 {
-	static Stage primaryStage, spaceStage = new Stage();
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
-		this.primaryStage=primaryStage;
 		primaryStage.setTitle("START");
 		primaryStage.getIcons().add(new Image("file:files/icon.png"));
 		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
-		new space.MainSpace().start(spaceStage);// call another window
 	}
-	public void handleAction(ActionEvent e) throws Exception {
-		primaryStage.close();//closing window
-		spaceStage.show();
+	@FXML private javafx.scene.control.Button closeButton;
+	@FXML public void closeWindow(ActionEvent e) throws Exception {
+		new space.MainSpace().start(new Stage());
+		Stage stage = (Stage) closeButton.getScene().getWindow();
+		stage.close();
 	}
 	public static void main(String[] args) {
 		launch(args);
